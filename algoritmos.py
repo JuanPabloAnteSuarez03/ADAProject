36fd5d190cf6552dde3c65f293fe4a3156ab200d
JuanPabloAnteSuarez03 <juan.pablo.ante@correounivalle.edu.co>
2024-06-11 01:22:13 -0500

    Se moidica las funciones para los equipos por rendimiento, y se crean la de los jugadores

--- algoritmos.py
+++ algoritmos.py
@@ -1,48 +1,24 @@
 from datos import *
 import statistics
-def equipoMayorRendimiento(Sedes, M):
-    mayor = 0
-    equipo = ""
-    ciudad = ""
-    for i in range(1, M+1):
-        mediaFutbol = []
-        mediaVolleyball = []
-        listaFutbol = Sedes[i]["Equipos"]["Futbol"]
-        listaVolleyball = Sedes[i]["Equipos"]["Volleyball"]
-        for j in listaFutbol:
-            mediaFutbol.append(Jugadores[j]["Rendimiento"])
-        for h in listaVolleyball:
-            mediaVolleyball.append(Jugadores[h]["Rendimiento"])
-        promedio_futbol = statistics.mean(mediaFutbol)
-        promedio_volleyball = statistics.mean(mediaVolleyball)        
-        maximo = max(promedio_futbol, promedio_volleyball)
-        if maximo > mayor:
-            mayor = maximo
-            equipo = "Futbol" if maximo == promedio_futbol else "Volleyball"
-            ciudad = Sedes[i]["Ciudad"]
-    return mayor, equipo, ciudad
+def equipoMayorRendimiento(Sedes, Jugadores, M):
+    arr = ordenarSedes(Sedes, Jugadores, M)
+    return arr[0][1][0][0] + " sede " + arr[0][0]
 
-def equipoMenorRendimiento(Sedes, M):
-    menor = float('inf')
-    equipo = ""
-    ciudad = ""
-    for i in range(1, M+1):
-        mediaFutbol = []
-        mediaVolleyball = []
-        listaFutbol = Sedes[i]["Equipos"]["Futbol"]
-        listaVolleyball = Sedes[i]["Equipos"]["Volleyball"]
-        for j in listaFutbol:
-            mediaFutbol.append(Jugadores[j]["Rendimiento"])
-        for h in listaVolleyball:
-            mediaVolleyball.append(Jugadores[h]["Rendimiento"])
-        promedio_futbol = statistics.mean(mediaFutbol)
-        promedio_volleyball = statistics.mean(mediaVolleyball)        
-        minimo = min(promedio_futbol, promedio_volleyball)
-        if minimo < menor:
-            menor = minimo
-            equipo = "Futbol" if minimo == promedio_futbol else "Volleyball"
-            ciudad = Sedes[i]["Ciudad"]
-    return menor, equipo, ciudad
+def equipoMenorRendimiento(Sedes, Jugadores, M):
+    arr = ordenarSedes(Sedes, Jugadores, M)
+    return arr[-1][-1][-1][0] + " sede " + arr[-1][0]
+
+def jugadorMayorRendimiento(Jugadores):
+    rendimientos = [(Jugadores[j]["Rendimiento"], j) for j in Jugadores]
+    rendimientos = quicksort(rendimientos)
+    jugadoresOrdenados = [j for _, j in rendimientos]
+    return (jugadoresOrdenados[-1], Jugadores[jugadoresOrdenados[-1]]["Nombre"], Jugadores[jugadoresOrdenados[-1]]["Rendimiento"])
+
+def jugadorMenorRendimiento(Jugadores):
+    rendimientos = [(Jugadores[j]["Rendimiento"], j) for j in Jugadores]
+    rendimientos = quicksort(rendimientos)
+    jugadoresOrdenados = [j for _, j in rendimientos]
+    return (jugadoresOrdenados[0], Jugadores[jugadoresOrdenados[0]]["Nombre"], Jugadores[jugadoresOrdenados[0]]["Rendimiento"])
 
 # Usaremos QuickSort como algoritmo de ordenamiento
 def quicksort(arr):
@@ -80,11 +56,9 @@ def ordenarJugadores(Sedes, Jugadores, M):
         for h in listaVolleyball:
             rendimientosVolleyball.append((Jugadores[h]["Rendimiento"], h))
         
-        # Ordenar las listas de rendimientos usando quicksort
         rendimientosFutbol = quicksort(rendimientosFutbol)
         rendimientosVolleyball = quicksort(rendimientosVolleyball)
         
-        # Actualizar las listas de jugadores en la sede con los jugadores ordenados
         Sedes[i]["Equipos"]["Futbol"] = [j for rendimiento, j in rendimientosFutbol]
         Sedes[i]["Equipos"]["Volleyball"] = [h for rendimiento, h in rendimientosVolleyball]
     
@@ -92,7 +66,6 @@ def ordenarJugadores(Sedes, Jugadores, M):
 
 
 def ordenarEquipos(Sedes, Jugadores, M):
-    # Ordenar los jugadores dentro de cada equipo
     Sedes = ordenarJugadores(Sedes, Jugadores, M)
     
     sedes_ordenadas = []
@@ -106,20 +79,16 @@ def ordenarEquipos(Sedes, Jugadores, M):
             promedio_rendimiento = statistics.mean(rendimientos)
             equipos_rendimiento.append((promedio_rendimiento, equipo, sede["Equipos"][equipo]))
         
-        # Ordenar los equipos por rendimiento promedio en orden descendente
         equipos_rendimiento = quicksort(equipos_rendimiento)
-        equipos_rendimiento.reverse()  # Invertir para tener orden descendente
+        equipos_rendimiento.reverse()  
         
-        # Crear una lista con los equipos ordenados
         equipos_ordenados = [(equipo, jugadores) for _, equipo, jugadores in equipos_rendimiento]
         
-        # Añadir la sede con los equipos ordenados a la lista
         sedes_ordenadas.append((sede["Ciudad"], equipos_ordenados))
     
     return sedes_ordenadas
 
 def ordenarSedes(Sedes, Jugadores, M):
-    # Ordenar los jugadores dentro de cada equipo
     Sedes = ordenarJugadores(Sedes, Jugadores, M)
     
     sedes_ordenadas = []
@@ -133,22 +102,22 @@ def ordenarSedes(Sedes, Jugadores, M):
             promedio_rendimiento = statistics.mean(rendimientos)
             equipos_rendimiento.append((promedio_rendimiento, equipo, sede["Equipos"][equipo]))
         
-        # Ordenar los equipos por rendimiento promedio en orden descendente
         equipos_rendimiento = quicksort(equipos_rendimiento)
-        equipos_rendimiento.reverse()  # Invertir para tener orden descendente
+        equipos_rendimiento.reverse()  
         
-        # Crear una lista con los equipos ordenados
         equipos_ordenados = [(equipo, jugadores) for _, equipo, jugadores in equipos_rendimiento]
         
-        # Añadir la sede con los equipos ordenados a la lista
         sedes_ordenadas.append((sede["Ciudad"], equipos_ordenados))
     
-    # Ordenar las sedes por el promedio de rendimiento de sus equipos
     sedes_ordenadas = quicksort(sedes_ordenadas)
-    sedes_ordenadas.reverse()  # Invertir para tener orden descendente
+    sedes_ordenadas.reverse()
     
     return sedes_ordenadas
 
-print(ordenarJugadores(Sedes, Jugadores, M))
-print(ordenarEquipos(Sedes, Jugadores, M))
-print(ordenarSedes(Sedes, Jugadores, M))
+# print(ordenarJugadores(Sedes, Jugadores, M))
+# print(ordenarEquipos(Sedes, Jugadores, M))
+# print(ordenarSedes(Sedes, Jugadores, M))
+# print(equipoMayorRendimiento(Sedes, Jugadores, M))
+# print(equipoMenorRendimiento(Sedes, Jugadores, M))
+print(jugadorMayorRendimiento(Jugadores))
+print(jugadorMenorRendimiento(Jugadores)) 
